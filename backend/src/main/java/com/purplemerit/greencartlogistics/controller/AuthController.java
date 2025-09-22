25ca6ebba13b0bc102c574919cdfa95fed68eb3f
SaurabhDixit <SaurabhDixit2202@gmail.com>
2025-09-22 10:33:04 +0530

    README Updates: GreenCart Logistics frontend & backend

--- backend/src/main/java/com/purplemerit/greencartlogistics/controller/AuthController.java
+++ backend/src/main/java/com/purplemerit/greencartlogistics/controller/AuthController.java
@@ -18,6 +18,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
